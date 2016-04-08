699849fafb0d114cdbb743dbd398ad4da6a881a5
YoesMonterroza <yoesmonterroza@hotmail.com>
2016-04-07 21:55:08 -0500

    ....

--- Finanzas_Generico/Conexion/Utilidades.cs
+++ Finanzas_Generico/Conexion/Utilidades.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-//editado
+//editado 2
 namespace Finanzas_Generico.Conexion
 {
     public static class Utilidades
