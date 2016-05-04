23dcd26c821dfb2196fd45d926aae0179dc5a8a9
jfbenavid <jfbenavid@hotmail.com>
2016-05-03 23:10:25 -0500

    se agrega un mensaje de entrada cuando le quedan 15 dias o menos de licencia al programa como informacion al usuario

--- Finanzas_Generico/MainWindow.xaml.cs
+++ Finanzas_Generico/MainWindow.xaml.cs
@@ -25,21 +25,35 @@ namespace Finanzas_Generico
             string pass = Utilidades.ConvertirHash(txtPassBox.Password);
             Usuario usuario = new Usuario();
             usuario = AdministrarUsuario.Consultar(user);
-            //if (usuario.licencia > DateTime.Now)
-            //{
             if (!usuario.resultado.Equals("error"))
             {
                 if (usuario.pass.Equals(pass))
                 {
+                    string mensaje = string.Empty;
+                    //se calcula el numero de dias restantes para finalizar la licencia para colocar un mensaje en el inicio
+                    double tiempoLicencia = DateTime.Now.Subtract((DateTime)usuario.licencia).TotalDays;
+                    if (tiempoLicencia <= 15)
+                    {
+                        string repetir = "por favor comuniquese con el proveedor del servicio para obtener una nueva.";
+                        if (tiempoLicencia > 1)
+                            mensaje = string.Format("Su licencia expirará dentro de {0} dias, {1}", tiempoLicencia, repetir);
+                        else if (tiempoLicencia == 1)
+                            mensaje = string.Format("Su licencia expira hoy, {0}", repetir);
+                        else
+                            mensaje = string.Format("Su licencia ha expirado, {0}", repetir);
+                    }
+
+                    //en el caso de que ya se haya terminado la licencia coloca este mensaje
                     if (usuario.licencia <= DateTime.Now)
                     {
                         MessageBox.Show("Su licencia ha expirado, por favor comuniquese con el proveedor del servicio\n\r" +
                                     "para obtener una nueva.", "Aviso");
                         Utilidades.licencia = false;
                     }
+
                     Utilidades.IdUsuario = usuario.id;
                     Utilidades.Usuario = usuario.nick;
-                    VentanaInicio vi = new VentanaInicio();
+                    VentanaInicio vi = new VentanaInicio(mensaje);
                     this.Hide();
                     vi.ShowDialog();
                     if (vi.DialogResult == true)
@@ -65,17 +79,10 @@ namespace Finanzas_Generico
                 MessageBox.Show("El usuario y/o la contraseña son incorrectos", "Error");
                 txtPassBox.Clear();
             }
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Su licencia ha expirado, por favor comuniquese con el proveedor del servicio\n\r" +
-            //        "para obtener una nueva.", "Aviso");
-            //}
         }
 
         private void lblRecuperaContrasena_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            //this.Hide();
             RecuperarContrasena rc = new RecuperarContrasena();
             rc.ShowDialog();
         }
